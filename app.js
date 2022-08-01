1abb26aba211915f0b55b14773072e58046748f0
mohd-Jainuddin <zainuddinshaikh3032@gmail.com>
2022-08-01 22:06:24 +0530

    Bubble sort

---
+++ app.js
@@ -0,0 +1,14 @@
+// Bubble sort
+
+function sort(arr) {
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1] ]= [arr[j + 1], arr[j]]
+            }
+        }
+    }
+    return arr
+}
+const result = sort([5, 3, 4, 1, 2, 8, 6, 7])
+console.log(result)
